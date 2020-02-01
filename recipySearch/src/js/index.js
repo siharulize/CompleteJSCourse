519dabca898f43842ecfb0a145a37493ad627abe
tatia <49821007+tatiasi@users.noreply.github.com>
2020-02-01 14:10:20 +0100

    recipe search website project

--- recipySearch/src/js/index.js
+++ recipySearch/src/js/index.js
@@ -1,6 +1,12 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
+import Likes from './models/Likes';
+
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
+import * as likesView from './views/likesView';
 import {elements, renderLoader, clearLoader} from './views/base';
 
 /** Global state of the app
@@ -11,6 +17,7 @@ import {elements, renderLoader, clearLoader} from './views/base';
  */
 
 const state = {};
+//window.state - state;  //for testing
 
 /**
  * SEARCH CONTROLLER
@@ -19,6 +26,8 @@ const state = {};
 const controlSearch = async () => {
     //1. get query from the view
     const query = searchView.getInput();
+    //TESTING ***********88888
+    //const query = 'pizza';
 
     if (query){
         // 2. new search object and add to state
@@ -29,12 +38,18 @@ const controlSearch = async () => {
         searchView.clearResults(); 
         renderLoader(elements.searchRes);
 
+        try {
         // 4. Search for recipies
         await state.search.getResults();
 
         // 5. render results on UI
         clearLoader();
         searchView.renderResults(state.search.result);
+        } catch {
+            alert('something went wrong!');
+            clearLoader();
+        }
+
 
     }
 
@@ -44,6 +59,12 @@ elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
+
+//TESTING *******************************************
+// window.addEventListener('load', e => {
+//     e.preventDefault();
+//     controlSearch();
+// });
 
 elements.searchResPages.addEventListener('click', e =>{
     const btn = e.target.closest('.btn-inline');
@@ -58,5 +79,158 @@ elements.searchResPages.addEventListener('click', e =>{
  * RECIPE CONTROLLER
  */
 
-const controlRecipe =() =>{
-    const id = window.location.has
+const controlRecipe =async () =>{
+    //Get the ID from the url
+    const id = window.location.hash.replace('#', '');
+    
+    if (id){
+        //prepare UI for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
+
+        //highlight selected search item
+        if (state.search) searchView.highlightSelected(id);
+
+        //create new recipe object
+        state.recipe = new Recipe(id);
+        
+        //TESTING ----------------------------------
+       // window.r = state.recipe;
+
+
+        try {
+        //get the recipe data
+        await state.recipe.getRecipe();
+        state.recipe.parseIngredients();
+
+        //calculate servings and time
+        state.recipe.calcTime();
+        state.recipe.calcServings();
+
+        //render the recipe
+        clearLoader();
+        recipeView.renderRecipe(
+            state.recipe,
+            state.likes.isLiked(id)
+            );
+
+        }
+        catch(error) {
+            alert('error processing recipe!');
+
+        }
+
+    }
+};
+
+//window.addEventListener('hashchange', controlRecipe);
+//window.addEventListener('load', controlRecipe);
+
+['hashchange', 'load'].forEach(e => window.addEventListener(e, controlRecipe));
+
+/**
+ * LIST CONTROLLER
+ */
+
+ const controlList = () => {
+     //create a nre list if there is none yet
+    if (!state.list) state.list = new List();
+
+    //add each ingredient to the list and YU
+    state.recipe.ingredients.forEach(el => {
+        const item = state.list.addItem(el.count, el.unit, el.ingredient);
+        listView.renderItem(item);
+    });
+ }
+
+ //Handle delete and update list item events
+ elements.shopping.addEventListener('click', e =>{
+    const id = e.target.closest('.shopping__item').dataset.itemid;
+
+    //handle the delete button
+    if (e.target.matches('.shopping__delete, .shopping__delete *')){
+        //delete from state
+        state.list.deleteItem(id);
+
+        //delete from UI
+        listView.deleteItem(id);
+
+        //handle the count update
+    } else if (e.target.matches('.shopping__count-value')){
+        const val = parseFloat(e.target.value, 10);
+        state.list.updateCount(id,val); 
+    }
+ });
+
+/**
+ * LIKE CONTROLLER
+ */
+
+
+ const controlLike = () => {
+    if (!state.likes) state.likes = new Likes();
+    const currentID = state.recipe.id;
+
+    //user has not yet liked current recipe
+    if(!state.likes.isLiked(currentID)){
+        //add like to the state
+        const newLike = state.likes.addLike(
+            currentID,
+            state.recipe.title,
+            state.recipe.author,
+            state.recipe.img
+        );
+        //toggle the like button
+        likesView.toggleLikeBtn(true);
+
+        //add like to UI list
+        likesView.renderLike(newLike);
+    //user has yet liked current recipe
+    }else {
+        //remove like to the state
+        state.likes.deleteLike(currentID);
+        //toggle the like button
+        likesView.toggleLikeBtn(false);
+
+        //remove like to UI list
+        likesView.deleteLike(currentID);
+    }
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
+ };
+
+//restore liked recipes on page loads
+window.addEventListener('load', () => {
+    state.likes = new Likes();
+
+    //restore likes
+    state.likes.readStorage();
+
+    //toggle like menu button
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+    //render the existing likes
+    state.likes.likes.forEach(like => likesView.renderLike(like));
+});
+
+
+//handling recipe button clicks
+elements.recipe.addEventListener('click', e => {
+    if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+        //decrease button is clicked
+        if (state.recipe.servings > 1 ){
+        state.recipe.updateServings('dec');
+        recipeView.updateServingsIngredients(state.recipe);
+        }
+    } else if (e.target.matches('.btn-increase, .btn-increase *')) {
+        //increase button is clicked
+        state.recipe.updateServings('inc');
+        recipeView.updateServingsIngredients(state.recipe);
+    } else if (e.target.matches('.recipe__btn-add, .recipe__btn-add *')){
+        //add ingredients to shopping list
+        controlList();
+    } else if(e.target.matches('.recipe__love, .recipe__love *')){
+        //like controller
+        controlLike();
+    }
+});
+
--- recipySearch/src/js/models/Recipe.js
+++ recipySearch/src/js/models/Recipe.js
@@ -27,6 +27,80 @@ export default class Recipe {
     }
 
     calcServings(){
-        this.serving = 4;
+        this.servings = 4;
+    }
+
+    parseIngredients(){
+        const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds'];
+        const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pounds'];
+        const units = [...unitsShort, 'kg', 'g'];
+
+        const newIngredients = this.ingredients.map(el =>{
+            // 1. uniform units
+            let ingredient = el.toLowerCase();
+            unitsLong.forEach((unit, i) => {
+                ingredient = ingredient.replace(unit, unitsShort[i]);
+            });
+
+            //2. remove parentheses
+            ingredient = ingredient.replace(/ *\([^)]*\) */g, ' ');
+
+            // 3. parse ingredients into count, unit and ingredient
+            const arrIng = ingredient.split(' ');
+            const unitIndex = arrIng.findIndex(el2 => units.includes(el2));
+
+            let objIng;
+            if (unitIndex>-1){
+                //there is a unit
+
+                //example 4 1/2 cups, arrcount is [4, 1/2]
+                //ex 4 cups. arrCount is [4]
+                const arrCount = arrIng.slice(0, unitIndex); 
+
+                let count;
+                if (arrCount.length === 1){
+                    count = eval(arrIng[0].replace('-', '+'));
+                } else {
+                     count = eval(arrIng.slice(0, unitIndex).join('+'));
+                }
+
+                objIng = {
+                    count,
+                    unit: arrIng[unitIndex],
+                    ingredient: arrIng.slice(unitIndex + 1).join(' ')
+                }
+
+            } else if (parseInt(arrIng[0], 10)){
+                //there is no unit, but the 1st element is a number
+                objIng ={
+                    count: parseInt(arrIng[0], 10),
+                    unit: '',
+                    ingredient: arrIng.slice(1).join(' ')
+                }
+            } else if (unitIndex==-1){
+                //there is no unit
+                objIng = {
+                    count: 1,
+                    unit: '',
+                    ingredient
+                }
+            }
+
+
+            return objIng;
+        });
+        this.ingredients = newIngredients;
+    }
+
+    updateServings(type){
+        //servings
+        const newServings = type === 'dec' ? this.servings - 1 : this.servings + 1;
+
+        //ingredients
+        this.ingredients.forEach(ing =>{
+            ing.count *= (newServings/this.servings);
+        })
+
+        this.servings = newServings;
     }
 }
